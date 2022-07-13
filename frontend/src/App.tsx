fa48b63b92bd36adb428b8510627a020ca0fdb8f
Luciano Fernandes <lucianoffernands@gmail.com>
2022-07-13 15:34:42 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,16 @@ import SalesCard from "./components/SalesCard";
 function App() {
   return (
     <>
-    <Header />
-    <main>
-      <section id="sales">
-        <div className="dsmeta-container">
-          <SalesCard />
-        </div>
-      </section>
-    </main>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
 export default App;
